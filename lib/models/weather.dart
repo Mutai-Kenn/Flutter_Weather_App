20ac8886105957a31f6b6d47c236e005ab652fad
Kenn Mutai <mutaikenn@gmail.com>
2021-03-17 14:29:19 +0200

    Created the Data provider layer and Repository layer

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -74,6 +74,32 @@ class Weather extends Equatable {
       case 'sn':
         state = WeatherCondition.snow;
         break;
+      case 'sl':
+        state = WeatherCondition.sleet;
+        break;
+      case 'h':
+        state = WeatherCondition.hail;
+        break;
+      case 't':
+        state = WeatherCondition.thunderstorm;
+        break;
+      case 'hr':
+        state = WeatherCondition.heavyRain;
+        break;
+      case 'lr':
+        state = WeatherCondition.lightRain;
+        break;
+      case 's':
+        state = WeatherCondition.showers;
+        break;
+      case 'hc':
+        state = WeatherCondition.heavyCloud;
+        break;
+      case 'c':
+        state = WeatherCondition.clear;
+        break;
+      default:
+        state = WeatherCondition.unknown;
     }
     return state;
   }
